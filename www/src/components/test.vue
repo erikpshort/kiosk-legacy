e7e242af29c6aafb47df0470ad6ac922800b8480
brawlingNavada <JCLacivita@gmail.com>
2017-03-02 16:38:42 -0700

    first pass at first two buttons kinda working.

--- www/src/components/test.vue
+++ www/src/components/test.vue
@@ -5,47 +5,44 @@
 
 
     <!-- Dropdown Trigger -->
-    <a id='btn01' class='dropdown-button btn red' href='#' data-activates='dropdown1'>Equipment Type</a>
+    <span id='btn01' class='dropdown-button btn red' href='#' data-activates='dropdown1'>Equipment Type</span>
 
     <!-- Dropdown Structure -->
     <ul id='dropdown1' class='dropdown-content'>
-      <li><span @click="A_Clicked('1')">Chainsaw</span></li>
-      <li><span @click="A_Clicked('2')">Two-Stroke</span></li>
-      <li><span @click="A_Clicked('3')">Four-Stroke</span></li>
+      <li><span @click="A_Clicked('Chainsaw')">Chainsaw</span></li>
+      <li><span @click="A_Clicked('Two-Stroke')">Two-Stroke</span></li>
+      <li><span @click="A_Clicked('Four-Stroke')">Four-Stroke</span></li>
     </ul>
 
 
-    <a id='btn02' class='dropdown-button btn red' href='#' data-activates='dropdown2'>Make</a>
+    <span id='btn02' class='dropdown-button btn red' href='#' data-activates='dropdown2'>Make</span>
 
     <!-- Dropdown Structure -->
     <ul id='dropdown2' class='dropdown-content'>
+    <!-- The contents of this dropdown are dynamically constructed based upon the choice from button 01 -->
+    </ul>
+
+    <a id='btn03' class='dropdown-button btn red' href='#' data-activates='dropdown3'>Model</a>
+
+    <ul id='dropdown3' class='dropdown-content'>
+      <li><span @click="B_Clicked('1')">two</span></li>
+      <li><span @click="B_Clicked('2')">two</span></li>
+      <li><span @click="B_Clicked('3')">three</span></li>
+    </ul>
+
+    <a id='btn04' class='dropdown-button btn red' href='#' data-activates='dropdown4'>Tune / Repair</a>
+
+    <!-- Dropdown Structure -->
+    <ul id='dropdown4' class='dropdown-content'>
       <li><span @click="B_Clicked('1')">one</span></li>
       <li><span @click="B_Clicked('2')">two</span></li>
       <li><span @click="B_Clicked('3')">three</span></li>
     </ul>
 
-    <a id='btn02' class='dropdown-button btn red' href='#' data-activates='dropdown2'>Model</a>
+    <a id='btn05' class='dropdown-button btn red' href='#' data-activates='dropdown5'>Regular / Rush</a>
 
     <!-- Dropdown Structure -->
-    <ul id='dropdown2' class='dropdown-content'>
-      <li><span @click="B_Clicked('1')">one</span></li>
-      <li><span @click="B_Clicked('2')">two</span></li>
-      <li><span @click="B_Clicked('3')">three</span></li>
-    </ul>
-
-        <a id='btn02' class='dropdown-button btn red' href='#' data-activates='dropdown2'>Tune / Repair</a>
-
-    <!-- Dropdown Structure -->
-    <ul id='dropdown2' class='dropdown-content'>
-      <li><span @click="B_Clicked('1')">one</span></li>
-      <li><span @click="B_Clicked('2')">two</span></li>
-      <li><span @click="B_Clicked('3')">three</span></li>
-    </ul>
-
-        <a id='btn02' class='dropdown-button btn red' href='#' data-activates='dropdown2'>Regular / Rush</a>
-
-    <!-- Dropdown Structure -->
-    <ul id='dropdown2' class='dropdown-content'>
+    <ul id='dropdown5' class='dropdown-content'>
       <li><span @click="B_Clicked('1')">one</span></li>
       <li><span @click="B_Clicked('2')">two</span></li>
       <li><span @click="B_Clicked('3')">three</span></li>
@@ -67,26 +64,59 @@
       }
     },
     methods: {
-      A_Clicked: function (code) {
-        console.log("Button A has been clicked with code: ", code)
-        var dom_but01 = document.getElementById("btn01")
+      makeLi: function(dropText){
+        console.log("in makeLi")
+        //Create the new list and span elements.
+        var new_li = document.createElement("li")
+        var new_span = document.createElement("span")
+        new_span.innerText = dropText
+        new_span.addEventListener("click", ()=> this.B_Clicked(dropText));
+        new_li.appendChild(new_span)
+        return new_li
+      },
 
-        if (code == '1') {
-          dom_but01.innerText = "Option One"
+      A_Clicked: function (code) {
+        var dom_but01 = document.getElementById("btn01")
+        var dom_but02 = document.getElementById("btn02")
+        var dom_but03 = document.getElementById("btn03")
+        var dom_but04 = document.getElementById("btn04")
+        var dom_but05 = document.getElementById("btn05")
+
+        console.log("Button A has been clicked with code: ", code)
+
+        if (code == 'Chainsaw') {
+          dom_but01.innerText = code;
+          //set the options for the make dropdown based upon the fact that we have selected chainsaw.
+          var ul02 = document.getElementById('dropdown2')
+          //we can add makes in this section.
+          ul02.appendChild(this.makeLi("Remmington"))
+          ul02.appendChild(this.makeLi("Makita"))
+          ul02.appendChild(this.makeLi("Polan"))
+          //change the color of the button to green to indicated a successful choice.
           dom_but01.setAttribute('class', 'dropdown-button btn green')
+
         }
         else if (code == '2') {
-          dom_but01.innerText = "Option Two"
+          dom_but01.innerText = "Two-stroke"
+
           dom_but01.setAttribute('class', 'dropdown-button btn green')
         }
         else if (code == '3') {
-          dom_but01.innerText = "Option Three"
+          dom_but01.innerText = "Four-stroke"
           dom_but01.setAttribute('class', 'dropdown-button btn green')
         }
         //console.log(dom_but01)
-      }
+      },
 
-  }
+
+      B_Clicked: function (code) {
+        var dom_but02 = document.getElementById("btn02")
+        console.log("Button B has been clicked to indicate make: ", code)
+        dom_but02.setAttribute('class', 'dropdown-button btn green')
+        dom_but02.innerText = code;
+        //console.log(dom_but02)
+      }
+    }
   }
 
 </script>
@@ -96,6 +126,7 @@
   .placeholder {
     font-size: 36px
   }
+
 
 
 
