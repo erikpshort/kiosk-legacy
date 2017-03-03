54695d8f3f5da312cbc94d5449825cf57e0f67ea
brawlingNavada <JCLacivita@gmail.com>
2017-03-03 14:03:11 -0700

    fixed problem of service request not reloading unless page refreshed and duplicate enteries being added to second dropdown button

--- www/src/main.js
+++ www/src/main.js
@@ -4,6 +4,9 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import $ from 'jquery'
+import materialize from  'materialize-css'
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
