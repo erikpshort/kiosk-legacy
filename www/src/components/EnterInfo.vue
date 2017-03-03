e8d05b59abf011bbb0415b1733b2d4ee92bffb12
erikpshort <erikshort@hotmail.com>
2017-03-03 13:11:33 -0700

    Hello to Store

--- www/src/components/EnterInfo.vue
+++ www/src/components/EnterInfo.vue
@@ -4,67 +4,68 @@
 
     <div class="container">
 
-    <form class="col s12">
-<div class="row">
-        <div class="input-field col s6 offset-s3 offset-s3">
-          <input type="text" name="Name" placeholder="Name" class="validate">
-          <!--<label for="icon_prefix">Company name</label>-->
+      <form class="col s12">
+        <div class="row">
+          <div class="input-field col s6 offset-s3 offset-s3">
+            <input type="text" name="Name" placeholder="Name" class="validate">
+            <!--<label for="icon_prefix">Company name</label>-->
+          </div>
         </div>
-      </div>
 
 
 
-      <div class="row">
-        <div class="input-field col s6 offset-s3 offset-s3">
-          <input type="text" name="CompanyName" placeholder="Company Name" class="validate">
-          <!--<label for="icon_prefix">Company name</label>-->
+        <div class="row">
+          <div class="input-field col s6 offset-s3 offset-s3">
+            <input type="text" name="CompanyName" placeholder="Company Name" class="validate">
+            <!--<label for="icon_prefix">Company name</label>-->
+          </div>
         </div>
-      </div>
-      <div class="row">
-        <div class="input-field col s6 offset-s3">
-          
-  <label for="phonenum">Phone Number (format: xxx-xxx-xxxx):</label><br/><br/>
-  <input id="phonenum" type="tel" class=" validate" placeholder=" xxx-xxx-xxxx" pattern="^\d{3}-\d{3}-\d{4}$"  >
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
 
-    <!--      <input type="tel" name="number" placeholder="XXX-XXX-XXXX" pattern="^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$">
+            <label for="phonenum">Phone Number (format: xxx-xxx-xxxx):</label><br/><br/>
+            <input v-if="!phone[0]" id="phonenum" type="tel" class=" validate" placeholder=" xxx-xxx-xxxx" pattern="^\d{3}-\d{3}-\d{4}$">
+            <input v-if="phone[0]" id="phonenum" type="tel" class=" validate" :value="phone" pattern="^\d{3}-\d{3}-\d{4}$">
+
+            <!--      <input type="tel" name="number" placeholder="XXX-XXX-XXXX" pattern="^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$">
           <input id="icon_telephone" type="tel" class="validate">
           <label for="icon_telephone"></label> -->
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="Street Address" class="validate">
-          <label for="icon_prefix"></label>
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <input id="icon_prefix" type="text" placeholder="Street Address" class="validate">
+            <label for="icon_prefix"></label>
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="City" class="validate">
-          <label for="icon_prefix"></label>
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <input id="icon_prefix" type="text" placeholder="City" class="validate">
+            <label for="icon_prefix"></label>
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="State" class="validate">
-          <label for="icon_prefix"></label>
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <input id="icon_prefix" type="text" placeholder="State" class="validate">
+            <label for="icon_prefix"></label>
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-        <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="Zip" class="validate">
-          <label for="icon_prefix"></label>
+        <div class="row">
+          <div class="input-field col s6 offset-s3">
+            <input id="icon_prefix" type="text" placeholder="Zip" class="validate">
+            <label for="icon_prefix"></label>
+          </div>
         </div>
-      </div>
 
-      <div class="row">
-  <button type = "submit">Submit</button>
-      </div>
+        <div class="row">
+          <button type="submit">Submit</button>
+        </div>
 
-    </form>
+      </form>
     </div>
 
   </div>
@@ -75,8 +76,34 @@
     name: 'EnterInfo',
     data() {
       return {
+        msg: 'Please Enter Your Information',
+        companyName: '',
+        phoneNumber: '',
+        streetAddress: '',
+        city: '',
+        state: '',
+        zip: '',
         msg: 'Please Enter Your Information'
+
       }
+    },
+    computed: {
+      phone() {
+        return this.$root.$data.store.state.activePhone
+      }
+    },
+     methods: {
+      returnCompanyInfo : function(){
+      console.log("--------------------------------")
+      console.log("Company Name:", this.companyName)
+      console.log("Phone Number:", this.phoneNumber)
+      console.log("Street Address:", this.streetAddress)
+      console.log("City", this.city)
+      console.log("State:", this.state)
+      console.log("Zip:", this.companyName)
+      console.log("--------------------------------")
+      this.$router.push('ConfirmCompanyInfo')
+      } 
     }
   }
 
@@ -87,20 +114,23 @@
   .validate {
     font-size: 36px
   }
-  
+
+
   #telephone {
     font-size: 36px;
     text-align: center;
   }
 
-::-webkit-input-placeholder {
- font-size: 36px;
- color: #d0cdfa;
- text-transform: uppercase;
- text-transform: uppercase;
- text-align: center;
-}
+  ::-webkit-input-placeholder {
+    font-size: 36px;
+    color: #d0cdfa;
+    text-transform: uppercase;
+    text-transform: uppercase;
+    text-align: center;
+  }
 
-input {text-align: center;}
 
+  input {
+    text-align: center;
+  }
 </style>
--- www/src/components/Hello.vue
+++ www/src/components/Hello.vue
@@ -1,35 +1,39 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-        <form class="col s12">
+    <form class="col s12">
       <div class="row">
-    
+
         <div class="input-field col s6 offset-s3">
           <!--<i class="material-icons prefix">phone</i>-->
-<label for="phonenum">Phone Number (format: xxx-xxx-xxxx):</label><br/><br/>
-  <input id="phonenum" type="tel" class=" validate" placeholder=" xxx-xxx-xxxx" pattern="^\d{3}-\d{3}-\d{4}$"  >
+          <label for="phonenum">Phone Number (format: xxx-xxx-xxxx):</label><br/><br/>
+          <form @submit.prevent="phoneNum(phone)"><input id="phonenum" type="tel" class=" validate" placeholder="XXX-XXX-XXXX" pattern="^\d{3}-\d{3}-\d{4}$" v-model="phone"></form>
 
 
 
-     <!--      <input type="tel" name="number" pattern="^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$">
+          <!--      <input type="tel" name="number" pattern="^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$">
           <input id="telephone" type="tel" class="validate">
           <label for="telephone"><span class="placeholder"></span></label>  -->
         </div>
       </div>
     </form>
 
- <div class="row">
-    <a class="waves-effect waves-light btn"><router-link :to="'/ServiceRequest'">ENTER</router-link></a>
+    <div class="row">
+      <a class="waves-effect waves-light btn" @click="phoneNum(phone)">ENTER</a>
     </div>
-     <div class="row">
-       <h4>or</h4>
+    <div class="row">
+      <h4>or</h4>
     </div>
-     <div class="row">
-    <a class="waves-effect waves-light btn"><router-link :to="'/companyDirectory'">Find By Company Name</router-link></a>
+    <div class="row">
+      <a class="waves-effect waves-light btn">
+        <router-link :to="'/companyDirectory'">Find By Company Name</router-link>
+      </a>
     </div>
 
-     <div class="row">
-    <a class="waves-effect waves-light btn"><router-link :to="'/EnterInfo'">Register as a New Company</router-link></a>
+    <div class="row">
+      <a class="waves-effect waves-light btn">
+        <router-link :to="'/EnterInfo'">Register as a New Customer</router-link>
+      </a>
     </div>
 
   </div>
@@ -40,8 +44,49 @@
     name: 'hello',
     data() {
       return {
-        msg: 'Please Enter Your Phone Number'
+        msg: 'Please Enter Your Phone Number',
+        phone: ''
       }
+    },
+    methods: {
+      phoneNum(phone) {
+        console.log(phone)
+        this.activeCustomers.forEach(customer => {
+          customer.cellPhone.forEach(num => {
+            if (num == phone) {
+              this.$root.$data.store.state.activeCustomer = customer
+              return this.$router.push('/CompanyInfo')
+            } else if (this.activeAdmins.forEach(admin => {
+              admin.cellPhone.forEach(num => {
+                if (num == phone) {
+                  this.$root.$data.store.state.activeAdmin = admin
+                  return this.$router.push('companyDirectory')
+                }
+              })
+            })){}
+            else{
+              this.$root.$data.store.state.activePhone = phone
+              return this.$router.push('EnterInfo')
+            }        
+          })
+        })
+      }
+    },
+    computed: {
+      activeAdmins() {
+        return this.$root.$data.store.state.activeAdmins
+      },
+      activeCustomers() {
+        return this.$root.$data.store.state.activeCustomers
+      },
+      activeJobs() {
+        return this.$root.$data.store.state.activeJobs
+      }
+    },
+    mounted() {
+      this.$root.$data.store.actions.getActiveAdmins()
+      this.$root.$data.store.actions.getActiveCustomers()
+      this.$root.$data.store.actions.getActiveJobs()
     }
   }
 
@@ -49,28 +94,28 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.placeholder {font-size: 36px}
-#telephone {font-size: 36px; text-align: center;}
-a {color: white}
-
-  .validate {
+  .placeholder {
     font-size: 36px
   }
-  
   #telephone {
     font-size: 36px;
     text-align: center;
   }
-
-::-webkit-input-placeholder {
- font-size: 36px;
- color: #d0cdfa;
- text-transform: uppercase;
- text-transform: uppercase;
- text-align: center;
-}
-
-input {text-align: center;}
-
-
+  .validate {
+    font-size: 36px
+  }
+  #telephone {
+    font-size: 36px;
+    text-align: center;
+  }
+   ::-webkit-input-placeholder {
+    font-size: 36px;
+    color: #d0cdfa;
+    text-transform: uppercase;
+    text-transform: uppercase;
+    text-align: center;
+   }
+  input {
+    text-align: center;
+  }
 </style>
