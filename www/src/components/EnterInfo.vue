2209fe446514d77c149eacc38c9910a1b96236d0
brawlingNavada <JCLacivita@gmail.com>
2017-03-03 10:18:47 -0700

    Set up console log output for enter new company form.

--- www/src/components/EnterInfo.vue
+++ www/src/components/EnterInfo.vue
@@ -7,7 +7,7 @@
     <form class="col s12">
       <div class="row">
         <div class="input-field col s6 offset-s3 offset-s3">
-          <input type="text" name="CompanyName" placeholder="Company Name" class="validate">
+          <input v-model="companyName" type="text" name="CompanyName" placeholder="Company Name" class="validate">
           <!--<label for="icon_prefix">Company name</label>-->
         </div>
       </div>
@@ -15,7 +15,7 @@
         <div class="input-field col s6 offset-s3">
           
   <label for="phonenum">Phone Number (format: xxx-xxx-xxxx):</label><br/><br/>
-  <input id="phonenum" type="tel" class=" validate" placeholder=" xxx-xxx-xxxx" pattern="^\d{3}-\d{3}-\d{4}$"  >
+  <input v-model="phoneNumber" id="phonenum" type="tel" class=" validate" placeholder=" xxx-xxx-xxxx" pattern="^\d{3}-\d{3}-\d{4}$"  >
 
     <!--      <input type="tel" name="number" placeholder="XXX-XXX-XXXX" pattern="^(?:\(\d{3}\)|\d{3})[- ]?\d{3}[- ]?\d{4}$">
           <input id="icon_telephone" type="tel" class="validate">
@@ -25,34 +25,34 @@
 
       <div class="row">
         <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="Street Address" class="validate">
+          <input v-model="streetAddress" id="icon_prefix" type="text" placeholder="Street Address" class="validate">
           <label for="icon_prefix"></label>
         </div>
       </div>
 
       <div class="row">
         <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="City" class="validate">
+          <input v-model="city" id="icon_prefix" type="text" placeholder="City" class="validate">
           <label for="icon_prefix"></label>
         </div>
       </div>
 
       <div class="row">
         <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="State" class="validate">
+          <input v-model="state" id="icon_prefix" type="text" placeholder="State" class="validate">
           <label for="icon_prefix"></label>
         </div>
       </div>
 
       <div class="row">
         <div class="input-field col s6 offset-s3">
-          <input id="icon_prefix" type="text" placeholder="Zip" class="validate">
+          <input v-model="zip" id="icon_prefix" type="text" placeholder="Zip" class="validate">
           <label for="icon_prefix"></label>
         </div>
       </div>
 
       <div class="row">
-  <button type = "submit">Submit</button>
+      <button @click="returnCompanyInfo()" class="waves-effect waves-light btn ">Submit</button>
       </div>
 
     </form>
@@ -66,8 +66,27 @@
     name: 'EnterInfo',
     data() {
       return {
-        msg: 'Please Enter Your Company Information'
+        msg: 'Please Enter Your Company Information',
+        companyName: '',
+        phoneNumber: '',
+        streetAddress: '',
+        city: '',
+        state: '',
+        zip: ''
       }
+    },
+    methods: {
+      returnCompanyInfo : function(){
+      console.log("--------------------------------")
+      console.log("Company Name:", this.companyName)
+      console.log("Phone Number:", this.phoneNumber)
+      console.log("Street Address:", this.streetAddress)
+      console.log("City", this.city)
+      console.log("State:", this.state)
+      console.log("Zip:", this.companyName)
+      console.log("--------------------------------")
+      
+      } 
     }
   }
 
