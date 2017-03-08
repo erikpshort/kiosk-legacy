a7ec37847de8c51003d59161f250a1bef9f42219
brawlingNavada <JCLacivita@gmail.com>
2017-03-08 14:31:44 -0700

    added comments to code on the AdminBoard.

--- www/src/components/AdminBoard.vue
+++ www/src/components/AdminBoard.vue
@@ -3,6 +3,7 @@
     <h1>{{ msg }}</h1>
     <div class="row">
       <div class="col s4 orange jobText">Orange (four-stroke) Jobs
+        <!--fourStroke is a method that returns a array of objects which we then v-for to display in this column.-->
         <div v-for="job in fourStroke(this.$root.store.state.activeJobs)" @click=addToWorking(job._id)>{{job.created}} {{job.make}} {{job.model}} {{job.notes}}</div>
       </div>
       <div class="col s4 green jobText">Green (two-stroke) Jobs
@@ -10,20 +11,14 @@
       </div>
       <div class="col s4 blue jobText">Blue Board Goes Here</div>
     </div>
-
     <div class="row">
       <div class="col s12 grey jobText">Jobs Being Worked On
         <div v-for="job in working(this.$root.store.state.activeJobs)" @click=removeFromWorking(job._id)>{{job.created}} {{job.make}} {{job.model}} {{job.notes}}</div>
       </div>
     </div>
-
   </div>
-
-
-
   </div>
 </template>
-
 <script>
   export default {
     name: 'adminBoard',
@@ -33,17 +28,19 @@
       }
     },
     methods: {
-      //takes in an array of job objects, and returns those where the type is four-stroke.
+      //takes in an array of job objects, and returns an array of only those where the type is four-stroke.
       fourStroke: function (arr_jobs) {
-        //console.log(arr_jobs)
         this.out_array = arr_jobs.filter(function (element) {
+          //for the moment if element.archive = false they the job is shown in their colored board.
+          //the element.archive value is set to 'working' if this job should be "moved" to the working board.
+          //eventaully this flagging should be done in the jobStatus property (we don't have test objects with this field right now.)
           if (element.type == "four Stroke" && element.archive == false) { return true }
           else { return false }
         })
         return this.out_array;
       },
       twoStroke: function (arr_jobs) {
-        //console.log(arr_jobs)
+      //takes in an array of job objects, and returns an array of only those where the type is two-stroke.
         this.out_array = arr_jobs.filter(function (element) {
           if (element.type == "two Stroke" && element.archive == false) { return true }
           else { return false }
@@ -51,44 +48,30 @@
         return this.out_array;
       },
       working: function (arr_jobs) {
-        //console.log(arr_jobs)
         this.out_array = arr_jobs.filter(function (element) {
           if (element.archive == "working") { return true }
           else { return false }
         })
         return this.out_array;
       },
-      addToWorking: function (data) {
-        console.log("In test function with: ", data)
-        //parse through the active jobs array to find the object with this id.
-        //console.log(this.$root.store.state.activeJobs)
+      addToWorking: function (jobId) {
+        //loop through the active jobs array to find the object with this id and change the job.archive field to 'working'
         var arr_activeJobs = this.$root.store.state.activeJobs
         for (var i = 0; i < arr_activeJobs.length; i++) {
           var job = arr_activeJobs[i];
-          //console.log(job._id)
-          if (job._id == data) {
-            console.log(job)
-            console.log("----")
+          if (job._id == jobId) {
             job.archive = 'working'
-            console.log(job)
-
           }
         }
       },
-      removeFromWorking: function (data){
-        console.log("In remove from working function with: ", data)
-        //parse through the active jobs array to find the object with this id.
-        //console.log(this.$root.store.state.activeJobs)
+      removeFromWorking: function (jobId){
+        //loop through the active jobs array to find the object with this id and change the job.archive field to false        
+        console.log("In remove from working function with: ", jobId)
         var arr_activeJobs = this.$root.store.state.activeJobs
         for (var i = 0; i < arr_activeJobs.length; i++) {
           var job = arr_activeJobs[i];
-          //console.log(job._id)
           if (job._id == data) {
-            console.log(job)
-            console.log("----")
             job.archive = false
-            console.log(job)
-
           }
         }
       }
@@ -98,9 +81,7 @@
         this.$root.$data.store.actions.getActiveJobs()
       }
     }
-
 </script>
-
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
   .jobText {
