384a77b6cc871de717ebde6c9473279f4c7bb601
brawlingNavada <JCLacivita@gmail.com>
2017-03-08 13:08:22 -0700

    added color to admin / jobs board

--- www/src/components/AdminBoard.vue
+++ www/src/components/AdminBoard.vue
@@ -2,12 +2,24 @@
   <div class="AdminBoard">
     <h1>{{ msg }}</h1>
     <div class="row">
-      <div class="col s4">Orange (four-stroke) Board
+      <div class="col s4 orange jobText">Orange (four-stroke) Jobs
         <div v-for="job in fourStroke(this.$root.store.state.activeJobs)">{{job.created}} {{job.make}} {{job.model}} {{job.notes}}</div>
       </div>
-      <div class="col s4">Green (two-stroke) Board<div v-for="job in twoStroke(this.$root.store.state.activeJobs)">{{job.created}} {{job.make}} {{job.model}} {{job.notes}}</div></div>
-      <div class="col s4">Blue Board Goes Here</div>
+      <div class="col s4 green jobText">Green (two-stroke) Jobs
+        <div v-for="job in twoStroke(this.$root.store.state.activeJobs)">{{job.created}} {{job.make}} {{job.model}} {{job.notes}}</div>
+      </div>
+      <div class="col s4 blue jobText">Blue Board Goes Here</div>
     </div>
+
+    <div class="row">
+      <div class="col s12 grey jobText">Jobs Being Worked On
+
+      </div>
+
+    </div>
+
+
+
   </div>
 </template>
 
@@ -16,7 +28,7 @@
     name: 'adminBoard',
     data() {
       return {
-        msg: 'This is the admin Board',
+        msg: 'This is the Admin Board',
       }
     },
     methods: {
@@ -29,7 +41,7 @@
         })
         return this.out_array;
       },
-        twoStroke: function (arr_jobs) {
+      twoStroke: function (arr_jobs) {
         //console.log(arr_jobs)
         this.out_array = arr_jobs.filter(function (element) {
           if (element.type == "two Stroke") { return true }
@@ -48,69 +60,13 @@
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  .placeholder {
-    font-size: 36px
-  }
+.jobText {color:black;
+height: 350px;
+
+}
 
 
 
 
 
-
-
-
-
-
-  #telephone {
-    font-size: 36px;
-    text-align: center;
-  }
-
-
-
-
-
-
-
-
-
-
-  .validate {
-    font-size: 36px
-  }
-
-
-
-
-
-
-
-
-
-
-  #telephone {
-    font-size: 36px;
-    text-align: center;
-  }
-
-   ::-webkit-input-placeholder {
-    font-size: 36px;
-    color: #d0cdfa;
-    text-transform: uppercase;
-    text-transform: uppercase;
-    text-align: center;
-  }
-
-
-
-
-
-
-
-
-
-
-  input {
-    text-align: center;
-  }
 </style>
